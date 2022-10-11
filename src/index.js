e12fa753cbbd0afc53c967f987d9be7dcfb7086a
Ajanitone <ajanitone@freenet.de>
2022-10-11 14:36:20 +0200

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom/client";
+import BootstrapCard from "./component/card/BootstrapCard.js";
 
 //create your function here
-function BootStrapCard() {
-  return (
-    <div class="card m-5">
-      <img
-        class="card-img-top"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg/800px-Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg"
-        alt="Card image cap"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Bob Dylan</h5>
-        <p class="card-text">
-          Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American
-          singer/songwriter, author, and artist who has been an influential
-          figure in popular music and culture for more than five decades.
-        </p>
-        <a
-          href="https://en.wikipedia.org/wiki/Bob_Dylan"
-          class="btn btn-primary"
-        >
-          Go to wikipedia
-        </a>
-      </div>
-    </div>
-  );
-}
-//remember to use ReactDOM.render to include the component into the website
 
-ReactDOM.render(<BootStrapCard />, document.querySelector("#root"));
+//remember to use ReactDOM.render to include the component into the website
+// const root = ReactDOM.createRoot(document.getElementById("myDiv"));
+// root.render(<BootstrapCard />);
+// const root = ReactDOM.createRoot(document.getElementById("myDiv"));
+
+ReactDOM.render(<BootstrapCard />, document.getElementById("myDiv"));
